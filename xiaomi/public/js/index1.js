58712e52a409ad59e9da2bfbe05e7e801022e419
liangliangshishui <2452575692@qq.com>
2020-07-24 17:09:28 +0800

    2020/7/24

--- xiaomi/public/js/index1.js
+++ xiaomi/public/js/index1.js
@@ -1,28 +1,28 @@
 $(function(){
-    $(".topbar-download").hover(
-        function(){
-            $(this).addClass("active");
-        },
-        function(){
-            $(this).removeClass("active");
-        }
-    )
-    $(".topbar-cart a").hover(
-        function(){
-            $(".topbar-cart").addClass("active");
-        },
-        function(){
-            $(".topbar-cart").removeClass("active");
-        }
-    )
-    $(".logo").hover(
-        function(){
-            $(this).addClass("active");
-        },
-        function(){
-            $(this).removeClass("active");
-        }
-    )
+    // $(".topbar-download").hover(
+    //     function(){
+    //         $(this).addClass("active");
+    //     },
+    //     function(){
+    //         $(this).removeClass("active");
+    //     }
+    // )
+    // $(".topbar-cart a").hover(
+    //     function(){
+    //         $(".topbar-cart").addClass("active");
+    //     },
+    //     function(){
+    //         $(".topbar-cart").removeClass("active");
+    //     }
+    // )
+    // $(".logo").hover(
+    //     function(){
+    //         $(this).addClass("active");
+    //     },
+    //     function(){
+    //         $(this).removeClass("active");
+    //     }
+    // )
     $(".search-text").focus(
         function(){
             $(this).addClass("border-info");
@@ -37,16 +37,16 @@ $(function(){
             $(".keyword-list").removeClass("active");
         }
     )
-    $(".nav-item").hover(
-        function(){
-            $(".site-header").addClass("active");
-            $(".search-text").trigger("blur");
-        },
-        function(){
-            $(".site-header").removeClass("active");
+    // $(".nav-item").hover(
+    //     function(){
+    //         $(".site-header").addClass("active");
+    //         $(".search-text").trigger("blur");
+    //     },
+    //     function(){
+    //         $(".site-header").removeClass("active");
             
-        }
-    )
+    //     }
+    // )
     // $("ul.list-category>li").hover(
     //     function(){
     //         $(this).addClass("active");
@@ -70,15 +70,45 @@ $(function(){
             $(this).parents(".xm-plain-box").find("ul.brick-list").eq(target).removeClass("hide").siblings().addClass("hide");
         }
     )
-    $("#follow-a-weixn").hover(
+    $(".header-nav .nav-item").hover(
         function(){
-            $("#follow-img-weixin").removeClass("hide");
+            $(this).addClass("active").siblings().removeClass("active");
+            $(".header-nav .item-children").addClass("active");
+            // console.log($(this).index());
+            var index=$(this).index();
+            // $(".header-nav .children-list").eq()
+            // console.log($(".header-nav .children-list").eq(index));
+            $(".header-nav .children-list").eq(index-1).css({"z-index":1});
+            $(".header-nav .children-list").eq(index-1).siblings().css({"z-index":0});
+            $(".search-text").trigger("blur");
+
+        },
+        function(e){
+            // $(this).removeClass("active");
+            var index=$(this).index();
+            $(".header-nav .item-children").removeClass("active");
+            // $(".header-nav .children-list").index($(this).index()).css({"z-index":"1"});
+            // $(".header-nav .children-list").eq(index-1).css({"z-index":0});
+        }
+    )
+        $(".header-nav .item-children").hover(
+        function(){
+            var index=$(".header-nav .nav-item.active").index();
+            // console.log(eq)
+            $(".header-nav .item-children").addClass("active");
+            $(".header-nav .children-list").eq(index-1).css({"z-index":1});
+            $(".header-nav .children-list").eq(index-1).siblings().css({"z-index":0});
+
         },
         function(){
-            $("#follow-img-weixin").addClass("hide");
+            var index=$(".header-nav .nav-item.active").index();
+            $(".header-nav .item-children").removeClass("active");
+            // $(".header-nav .children-list").eq(index-1).css({"z-index":0});
             
         }
     )
+
+
         var vm=new Vue({
             el: '#app',
             data: {
